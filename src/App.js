5d5b49e1a4d4a4d3a6dbf0efb0f184cba37630ac
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-04-18 14:15:07 +0200

    .

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
     var scrollLimit = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
       document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
     window.scrollTo(0, scrollLimit + 10000);
+    setTimeout(function(){alert("Hello")}, 2000);
     // setTodos([]);
     // localStorage.setItem('todos', JSON.stringify([]));
   }
