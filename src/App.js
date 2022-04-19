735a2b094bdea45a32b6cff9e7b850fedc887cd6
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-04-19 17:43:50 +0200

    .

--- src/App.js
+++ src/App.js
@@ -119,12 +119,11 @@ function App() {
         aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
+          {"Confirmation"}
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
+            Are you sure to clear the list?
           </DialogContentText>
         </DialogContent>
         <DialogActions>
